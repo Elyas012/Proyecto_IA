d49e0c1dc818d7ce9f5b01088825f25d0e6d0b30
Jekken12 <johnguerrero7576@gmail.com>
2025-12-08 01:11:39 -0500

    Merge master actualizado en feature/mongodb

---
+++ fronted_nextjs/pages/api/test-mongo.ts
@@ -0,0 +1,24 @@
+    // fronted_nextjs/pages/api/test-mongo.ts
+    import type { NextApiRequest, NextApiResponse } from "next";
+    import { dbConnect } from "../../lib/mongodb";
+    import { Test } from "../../models/Test";
+
+
+    export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+    ) {
+    await dbConnect();
+
+    if (req.method === "POST") {
+        const doc = await Test.create({ message: "Hola Mongo Atlas" });
+        return res.status(201).json(doc);
+    }
+
+    if (req.method === "GET") {
+        const docs = await Test.find();
+        return res.status(200).json(docs);
+    }
+
+    return res.status(405).end();
+    }
