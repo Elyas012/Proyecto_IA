0af34a7c67b4ab9e06043cb33596e1773f7fcefc
Elias <eliasjacome77@gmail.com>
2026-02-02 11:44:00 -0500

    Use Railway API URL

--- fronted_nextjs/lib/api.ts
+++ fronted_nextjs/lib/api.ts
@@ -1,7 +1,9 @@
 // lib/api.ts
 import axios from "axios";
 
-export const BASE_URL = "http://127.0.0.1:8000/api";
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "https://proyectoia-production.up.railway.app/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
